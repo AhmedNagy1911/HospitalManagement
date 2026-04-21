de3cded3d36ff5fa31845c4ef36040ecb59908b9
AhmedNagy1911 <zamalekahmed196>
2026-04-22 01:29:01 +0200

    ..

--- HospitalManagement.API/Program.cs
+++ HospitalManagement.API/Program.cs
@@ -85,35 +85,6 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
 // ?? Swagger with JWT support ??????????????????????????????????
-//builder.Services.AddSwaggerGen(c =>
-//{
-//    c.SwaggerDoc("v1", new OpenApiInfo { Title = "HospitalManagement.API", Version = "v1" });
-
-//    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-//    {
-//        Name = "Authorization",
-//        Type = SecuritySchemeType.ApiKey,
-//        Scheme = "Bearer",
-//        BearerFormat = "JWT",
-//        In = ParameterLocation.Header,
-//        Description = "Enter: Bearer {your token}"
-//    });
-
-//    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-//    {
-//        {
-//            new OpenApiSecurityScheme
-//            {
-//                Reference = new OpenApiReference
-//                {
-//                    Type = ReferenceType.SecurityScheme,
-//                    Id   = "Bearer"
-//                }
-//            },
-//            Array.Empty<string>()
-//        }
-//    });
-//});
 
 // Dependency Injection
 builder.Services.AddApplication();
